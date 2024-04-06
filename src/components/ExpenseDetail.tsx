50e85e074573adcc2d36a179a66b6d8327ae6095
Layla Rodas <rodas.layla@gmail.com>
2024-04-06 23:57:07 +0200

    removing an item from the expense list

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,8 +1,17 @@
 import { useMemo } from "react"
+import {
+    LeadingActions,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions,
+    SwipeableList
+} from "react-swipeable-list"
 import { formatDate } from "../helpers"
 import { Expense } from "../types"
 import { AmountDisplay } from "./AmountDisplay"
 import { categories } from "../database/categories"
+import "react-swipeable-list/dist/styles.css"
+import { useBudget } from "../hooks/useBudget"
 
 
 type ExpenseDetailProps = {
@@ -10,25 +19,59 @@ type ExpenseDetailProps = {
 }
 
 
-export const ExpenseDetail = ({expense} : ExpenseDetailProps) => {
+export const ExpenseDetail = ({ expense }: ExpenseDetailProps) => {
 
-    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0] , [expense] )
+    const { dispatch } = useBudget();
 
-  return (
-    <div className=" bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
-        <div>
-            <img src={`/${categoryInfo.icon}.svg`} alt="icon" className="w-20" />
-        </div>
+    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
 
-        <div className="space-y-2 flex-1 ">
-            <p className=" text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
-            <p className="text-md font-semibold">{expense.expenseName}</p>
-            <p className=" text-slate-600 text-sm">{formatDate(expense.date!.toString() )}</p>
-        </div>
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction
+                onClick={() => alert("Edit")}
+            >
+                Update
+            </SwipeAction>
+        </LeadingActions>
+    )
 
-        <AmountDisplay 
-            amount={expense.amount}
-        />
-    </div>
-  )
+
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction
+                onClick={() => dispatch({ type: 'delete-expense', payload: { id: expense.id } })}
+                destructive={true}
+            >
+                Delete
+            </SwipeAction>
+        </TrailingActions>
+    )
+
+
+
+    return (
+        <SwipeableList>
+            <SwipeableListItem 
+                maxSwipe={30}
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+            >
+                <div className=" bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+                    <div>
+                        <img src={`/${categoryInfo.icon}.svg`} alt="icon" className="w-20" />
+                    </div>
+
+                    <div className="space-y-2 flex-1 ">
+                        <p className=" text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
+                        <p className="text-md font-semibold">{expense.expenseName}</p>
+                        <p className=" text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+                    </div>
+
+                    <AmountDisplay
+                        amount={expense.amount}
+                    />
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
+    )
 }
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -6,7 +6,8 @@ export type BudgetActions =
     { type: 'add-budget', payload:{ budget: number }} |
     { type: 'show-modal'} |
     { type: 'hide-modal'} |
-    { type: 'add-expense', payload: {expense: DraftExpense} }
+    { type: 'add-expense', payload: {expense: DraftExpense} } |
+    { type: 'delete-expense', payload: {id: Expense['id']} }
 
 
 export type BudgetState = {
@@ -61,6 +62,13 @@ export const budgetReducer = (
             ...state,
             expenses: [...state.expenses, expense],
             modal: false
+        }
+    }
+
+    if (action.type === 'delete-expense') {
+        return {
+            ...state,
+            expenses: state.expenses.filter(expense => expense.id !== action.payload.id)
         }
     }
 
