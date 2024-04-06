ecdd8841e4447cd5ddd87f390274254d084e4ef0
Layla Rodas <rodas.layla@gmail.com>
2024-04-06 20:10:40 +0200

    creating Context

---
+++ src/context/budgetContext.tsx
@@ -0,0 +1,24 @@
+import { useReducer, createContext, Dispatch, ReactNode } from "react"
+import { budgetReducer, initialState, BudgetActions, BudgetState  } from "../reducers/budget-reducer"
+
+type BudgetContextProps = {
+    state: BudgetState
+    dispatch: Dispatch<BudgetActions>
+}
+
+type BudgetProviderProps = {
+    children: ReactNode
+}
+
+export const BudgetContext = createContext<BudgetContextProps>(null!)
+
+export const BudgetProvider = ({children}: BudgetProviderProps) => {
+    
+    const [state, dispatch] = useReducer(budgetReducer, initialState)
+
+    return (
+        <BudgetContext.Provider value={{ state, dispatch }}>
+            {children}
+        </BudgetContext.Provider>
+    )
+}
