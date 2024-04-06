15b6646b03f7eae81706bbdceb71b8ac2e412d43
Layla Rodas <rodas.layla@gmail.com>
2024-04-07 00:30:55 +0200

    save data to LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import { BudgetForm } from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import { BudgetTracker } from "./components/BudgetTracker";
@@ -10,6 +10,13 @@ function App() {
   const { state } = useBudget();
 
   const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
+
+  useEffect(() => {
+    localStorage.setItem('budget', state.budget.toString())
+    localStorage.setItem('expenses', JSON.stringify(state.expenses))
+  
+  },[state])
+
 
   return (
     <>
